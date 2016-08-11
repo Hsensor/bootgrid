0e758a92fedd87a5da8f5b3830335166898512f1
Hsensor <1203460788@qq.com>
2016-08-11 19:28:54 +0800

    table header

--- bootgrid.js
+++ bootgrid.js
@@ -1,35 +1,61 @@
+;(function($){
 
-;(function (factory) {
-    if ( typeof define === 'function' && define.amd ) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else if (typeof exports === 'object') {
-        // Node/CommonJS style for Browserify
-        module.exports = factory;
-    } else {
-        // Browser globals
-        factory(jQuery);
-    }
-}(function ($) {
-    $fn.extend({
-        bootgrid:bootgridWrap
-    });
+    function table(opts){
+        var $ele = $(opts.selector);
 
-    function bootgridWrap(options){
-        new bootgrid(this,options);
+        if(!opts.selector||!($ele.length==1&&$ele[0].tagName==="TABLE")){
+            throw new Error("expected opts has an attribute called selector , $(opts.selector).length==1 and $(opts.selector)[0].tagName===\"TABLE\"");
+        }
+        
+        this.options = opts;
+        this.$ele = $ele;
+
+        this.init();
+    };
+
+    table.prototype.init = function() {
+        this.renderHeaderRow();
+
         return this;
-    }
-    function bootgrid(ele,options){
-        this.$ele = ele;
+    };
+    
+    table.prototype.renderHeaderRow = function() {
+        var $ele = this.$ele;
+        var $headerRow = $('<tr></tr>');
+        if(this.options.dealWithHeaderRow && $.type(this.options.dealWithHeaderRow)==="function"){
+            this.options.dealWithHeaderRow($headerRow);
+        }
+
+        $ele.append("<thead></thead>").find("thead").append($headerRow);
+        if(!this.options.columnDefs || $.type(this.options.columnDefs)!=="array"){
+            throw new Error("expected table columnDefs attribute was given and columnDefs is an array");
+        }
+        $.map(this.options.columnDefs,$.proxy(function(column){
+            this.renderHeaderCell($headerRow,column);
+        },this));
+        return this;
     };
 
-    bootgrid.prototype.renderHeader = function(){
-
+    table.prototype.renderHeaderCell = function($headerRow,column) {
+        if(column.headerCellTemplateFn && $.type(column.headerCellTemplateFn)==="function"){
+            var headerCellTempate = column.headerCellTemplateFn(column);
+            if(!headerCellTempate){
+                throw new Error("expected headerCellTemplateFn function return html view or jquery object");
+            }
+            $headerRow.append($("<td></td>").append(headerCellTempate))
+        }else if(column.headerCellTemplate){
+           $headerRow.append($("<td></td>").append(column.headerCellTemplate));
+        }else if(column.headerTitle){
+            $headerRow.append($("<td></td>").append(column.headerTitle));
+        }else{
+            throw new Error("expected your define column have an attribute called headerTitle|headerCellTempate|headerCellTemplateFn");
+        }
+        return this;
     };
-    bootgrid.prototype.renderBody = function(){
 
+    table.prototype.renderBody = function() {
+        return this;
     };
-    bootgrid.prototype.loadData = function(){
 
-    }
-}));
+    $.table = table;
+})(jQuery);
